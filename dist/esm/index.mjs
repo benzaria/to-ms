7b1501c400108cce05b6f6596629b1ecee1026ea
benz <benz.github@gmail.com>
2025-04-08 22:44:43 +0100

    Change ReturnType from void to undefined

---
+++ dist/esm/index.mjs
@@ -0,0 +1,21 @@
+// src/index.ts
+var msVal = { ms: 1, s: 1e3, m: 6e4, h: 36e5, d: 864e5, w: 6048e5, M: 2629746e3, y: 315576e5 };
+function tms(input, catchError = !1) {
+  if (typeof input == "string") {
+    let match = input.match(/^\s*([-+]?\s*\d*\.?\d+)\s*(ms|[smhdwMy]?)\s*$/);
+    if (!match) {
+      let formatError = "Invalid time format, support:" + JSON.stringify(Object.keys(msVal));
+      if (!catchError) throw new Error(formatError);
+      return console.error(formatError), void 0;
+    }
+    return +match[1] * (msVal[match[2]] ?? 1);
+  } else if (typeof input == "number")
+    return console.warn("The Input was a number, no need for converting"), input;
+  let typeError = `Input must be a string or number. Received: ${input}, with typeof ${typeof input}`;
+  if (!catchError) throw new TypeError(typeError);
+  return console.error(typeError), void 0;
+}
+export {
+  tms as default,
+  msVal
+};
